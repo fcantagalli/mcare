378abfd1957bb5edac69725fd015bdcf54774981
Philippe Ehlert <philippe.ehlert@gmail.com>
2013-06-06 15:44:48 -0300

    Adicionadas classes de realizar consulta

---
+++ mCare/src/com/mCare/consulta/realizarConsulta/RealizarConsultaMain.java
@@ -0,0 +1,124 @@
+package com.mCare.consulta.realizarConsulta;
+
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.mCare.R;
+import com.mCare.configuracaoConsulta.SelecionaCamposView;
+
+public class RealizarConsultaMain extends FragmentActivity implements ActionBar.TabListener {
+	
+	AppSectionsPagerAdapter mAppSectionsPagerAdapter;
+    ViewPager mViewPager;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		mAppSectionsPagerAdapter = new AppSectionsPagerAdapter(getSupportFragmentManager());
+		final ActionBar actionBar = getActionBar();
+		actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+		actionBar.setCustomView(R.layout.action_bar_main);
+		actionBar.setDisplayShowHomeEnabled(true);
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		View homeIcon = findViewById(android.R.id.home);
+		((View) homeIcon.getParent()).setVisibility(View.GONE);
+		
+		mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mAppSectionsPagerAdapter);
+        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                actionBar.setSelectedNavigationItem(position);
+            }
+        });
+        
+        for (int i = 0; i < mAppSectionsPagerAdapter.getCount(); i++) {
+            actionBar.addTab(actionBar.newTab().setText(mAppSectionsPagerAdapter.getPageTitle(i)).setTabListener(this));
+        }
+		
+	}
+	
+	@Override
+	public void onTabReselected(Tab tab, android.app.FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void onTabSelected(Tab tab, android.app.FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		mViewPager.setCurrentItem(tab.getPosition());
+	}
+
+	@Override
+	public void onTabUnselected(Tab tab, android.app.FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	@Override
+	public boolean onMenuItemSelected(int featureId, MenuItem item) {
+		switch(featureId){
+		case 0: {
+			Intent intent = new Intent(getApplicationContext(), SelecionaCamposView.class);
+			startActivity(intent);
+		}
+		}
+		return super.onMenuItemSelected(featureId, item);
+	}
+
+	
+	public static class AppSectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public AppSectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            switch (i) {
+                //case 0: return new Agenda_Fragment();
+                
+                //case 1: return new ListaPacientes();
+                
+                //case 2: return new Consultas_Fragment();
+            }
+            return new com.mCare.main.Agenda_Fragment();
+        }
+
+        @Override
+        public int getCount() {
+            return 4;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+        	switch(position){
+        	case 0: return "Consulta";
+        	case 1: return "Pacientes";
+        	case 2: return "Consultas";
+        	}
+        	
+            return "ERROR";
+        }
+    }
+	
+}
