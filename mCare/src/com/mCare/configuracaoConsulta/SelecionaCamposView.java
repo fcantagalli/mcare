d97e1f584438a495bfea03b7b8aaaf9bd874182c
Philippe Ehlert <philippe.ehlert@gmail.com>
2013-06-06 16:00:26 -0300

    Removido warning

--- mCare/src/com/mCare/configuracaoConsulta/SelecionaCamposView.java
+++ mCare/src/com/mCare/configuracaoConsulta/SelecionaCamposView.java
@@ -12,7 +12,6 @@ import android.view.View;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
