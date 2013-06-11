7f6705bb9e5e1067c7a1677b67cb47a1dd4ede14
Gabriela Scardine <gabrielascardine@gmail.com>
2013-06-10 21:55:40 -0300

    Modificadas telas de exames e helper/editar diagnostico

--- mCare/src/com/mCare/exame/ListaExames.java
+++ mCare/src/com/mCare/exame/ListaExames.java
@@ -1,21 +1,25 @@
-package com.mCare;
+package com.mCare.exame;
+
+import com.mCare.R;
+import com.mCare.R.layout;
+import com.mCare.R.menu;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class VisualizarExame extends Activity {
+public class ListaExames extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_visualizar_exame);
+		setContentView(R.layout.activity_lista_exames);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.visualizar_exame, menu);
+		getMenuInflater().inflate(R.menu.lista_exames, menu);
 		return true;
 	}
 
