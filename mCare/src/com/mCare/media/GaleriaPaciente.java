05c4da6f1aa88dff80a4e3308f4265084fe4fd66
Philippe Ehlert <philippe.ehlert@gmail.com>
2013-06-17 16:58:58 -0300

    Deletado arquivos inuteis e correcao de bugs

--- mCare/src/com/mCare/media/GaleriaPaciente.java
+++ mCare/src/com/mCare/media/GaleriaPaciente.java
@@ -1,23 +1,25 @@
-package com.mCare.consulta;
+package com.mCare.media;
 
 import com.mCare.R;
+import com.mCare.R.layout;
+import com.mCare.R.menu;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class VisualizarConsultaIndividual extends Activity {
+public class GaleriaPaciente extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_consulta_individual);
+		setContentView(R.layout.activity_galeria_paciente);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.consulta_individual, menu);
+		getMenuInflater().inflate(R.menu.galeria_paciente, menu);
 		return true;
 	}
 
