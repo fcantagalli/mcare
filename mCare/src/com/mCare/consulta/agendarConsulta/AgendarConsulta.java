06fbf7d1cd2bf59825531ea27d00cc5f6a489b35
Felipe Tozato Cantagalli <felipe.cantagalli@gmail.com>
2013-06-18 11:58:47 -0300

    tentei arrumar a notificacao mas nao esta funcionando

--- mCare/src/com/mCare/consulta/agendarConsulta/AgendarConsulta.java
+++ mCare/src/com/mCare/consulta/agendarConsulta/AgendarConsulta.java
@@ -157,7 +157,7 @@ public class AgendarConsulta extends Activity {
 		// 3600000 equivale a 1 hora de antecedencia
 		Log.i("fe","qual a string de tempo : "+dbConsulta.dbhelper.formataData(calendar));
 		NotificacaoConsulta notificacao = new NotificacaoConsulta();
-		notificacao.create(this,(calendar.getTimeInMillis()) , "Paciente: " + consulta.getPaciente().getNome(), "Consulta agendada", "Você tem uma consulta às "+consulta.getHora().get(Calendar.HOUR_OF_DAY)+" : "+consulta.getHora().get(Calendar.MINUTE)+" \n no endereço: " +consulta.getPaciente().getLogradouro() + ", "+consulta.getPaciente().getNumero(), R.drawable.ic_launcher);
+		notificacao.create(this,(calendar.getTimeInMillis()+3600000) , "Paciente: " + consulta.getPaciente().getNome(), "Consulta agendada", "Você tem uma consulta às "+consulta.getHora().get(Calendar.HOUR_OF_DAY)+" : "+consulta.getHora().get(Calendar.MINUTE)+" \n no endereço: " +consulta.getPaciente().getLogradouro() + ", "+consulta.getPaciente().getNumero(), R.drawable.ic_launcher);
 		
 		Toast.makeText(getApplicationContext(), "Consulta agendada com sucesso!", Toast.LENGTH_LONG).show();
 		
--- mCare/src/com/mCare/notificacao/NotificacaoConsulta.java
+++ mCare/src/com/mCare/notificacao/NotificacaoConsulta.java
@@ -1,5 +1,6 @@
 package com.mCare.notificacao;
 
+import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
@@ -8,17 +9,13 @@ import com.mCare.main.MainActivity;
 import android.annotation.SuppressLint;
 import android.app.AlarmManager;
 import android.app.Notification;
-import android.app.Notification.Builder;
 import android.app.Activity;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.ViewDebug.FlagToString;
 
 public class NotificacaoConsulta extends BroadcastReceiver {
 	
@@ -31,16 +28,6 @@ public class NotificacaoConsulta extends BroadcastReceiver {
 	@SuppressWarnings("deprecation")
 	@Override
 	public void onReceive(Context context, Intent paramIntent) {
-		// Request the notification manager
-		NotificationManager nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-
-		// Create a new intent which will be fired if you click on the notification
-		Intent notificationIntent = new Intent(context, MainActivity.class);
-
-		// Attach the intent to a pending intent
-		PendingIntent contentIntent = PendingIntent.getActivity(context, 0, notificationIntent, 0);
-
-		// Create the notification
 
 		Bundle b = paramIntent.getExtras();
 		String tickerText = b.getString("ticketText");
@@ -49,33 +36,33 @@ public class NotificacaoConsulta extends BroadcastReceiver {
 		int icon = b.getInt("icon");
 		int id = b.getInt("id");
 
-		Notification notification = null;
-		int apilevel = Build.VERSION.SDK_INT;
+		//Pending Intent para executar a intent ao selecionar a notificacao
 		
-		if(apilevel >= 11){
-			Builder builder = new Notification.Builder(context).setContentTitle(tickerText).setContentText(message).setSmallIcon(icon).setContentIntent(contentIntent).setAutoCancel(true);
-			if(apilevel >= 17){
-				//Android 4.2
-				notification = builder.build();
-				notification.defaults = notification.DEFAULT_ALL;
-			}else{
-				// Android 3.x
-				notification = builder.getNotification();
-				notification.defaults = notification.DEFAULT_ALL;
-			}
-		}
-		else{
-			//Android 2.2
-			notification = new Notification(icon,tickerText,id);
-			//Informacoes
-			notification.setLatestEventInfo(context, title, message, contentIntent);
-			notification.defaults = notification.DEFAULT_ALL;
-		}
-		
-		nm.notify(id,notification);
+				NotificationManager nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
+				// Create a new intent which will be fired if you click on the notification
+				Intent notificationIntent = new Intent(context, MainActivity.class);
+
+				// Attach the intent to a pending intent
+				PendingIntent contentIntent = PendingIntent.getActivity(context, 0, notificationIntent, 0);
+
+				// Create the notification
+
+				Notification notification = null;
+				
+				notification = new Notification.Builder(context)
+				.setTicker(tickerText)
+				.setContentTitle(title)
+				.setContentText(message)
+				.setSmallIcon(icon)
+				.setContentIntent(contentIntent)
+				.setAutoCancel(true)
+				.build();
+					
+				notification.flags |= Notification.FLAG_AUTO_CANCEL;	
+				
+				nm.notify(id,notification);
 	}
-
 	
 	@SuppressWarnings("deprecation")
 	@SuppressLint("NewApi")
@@ -103,13 +90,29 @@ public class NotificacaoConsulta extends BroadcastReceiver {
 			setContentIntent(contentIntent)
 			.setAutoCancel(true)
 			.setWhen(quandoIraAparecer)
-			.setShowWhen(true).build();
+			.build();
 			
 		notification.flags |= Notification.FLAG_AUTO_CANCEL;	
 		
 		nm.notify(id,notification);
-
 		
+		/*
+		AlarmManager gerenciador = (AlarmManager) contexto.getSystemService(Context.ALARM_SERVICE);
+		Calendar cal = Calendar.getInstance();
+		
+		cal.setTimeInMillis(quandoIraAparecer);
+		
+		Intent i = new Intent(contexto, MainActivity.class);
+		i.putExtra("ticketText", tickerText);
+		i.putExtra("title", title);
+		i.putExtra("message",message);
+		i.putExtra("icon", icon);
+		i.putExtra("id",quandoIraAparecer);
+		
+		PendingIntent p = PendingIntent.getBroadcast(contexto, 0, i, 0);
+		
+		gerenciador.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), p);
+		*/
 	}
 	
 	public void cancell(Context contexto, int id){
