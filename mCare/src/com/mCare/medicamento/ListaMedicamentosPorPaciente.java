defaef7391f188b1b09a4fae9d513104fa0b30df
Gabriela Scardine <gabrielascardine@gmail.com>
2013-06-21 14:29:28 -0300

    Adicionada lista de exames aa aba Dados Adicionais da consulta (mas ainda sem editText)

--- mCare/src/com/mCare/medicamento/ListaMedicamentosPorPaciente.java
+++ mCare/src/com/mCare/medicamento/ListaMedicamentosPorPaciente.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
@@ -11,11 +12,14 @@ import android.content.Context;
 import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.InputType;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
 import android.widget.ListView;
@@ -27,9 +31,11 @@ import com.mCare.consulta.realizarConsulta.ExpandableAdapter;
 import com.mCare.consulta.realizarConsulta.GroupEntity;
 import com.mCare.db.DbHelperDiagnostico;
 import com.mCare.db.DbHelperDiagnostico_Consulta;
+import com.mCare.db.DbHelperExame;
 import com.mCare.db.DbHelperMedicamento;
 import com.mCare.db.DbHelperMedicamento_Paciente;
 import com.mCare.diagnostico.Diagnostico;
+import com.mCare.exame.Exame;
 import com.mCare.paciente.Paciente;
 
 public class ListaMedicamentosPorPaciente extends Fragment {
@@ -52,20 +58,31 @@ public class ListaMedicamentosPorPaciente extends Fragment {
 		
 		Paciente p = new Paciente(id,nome_paciente);
 		
-		exList = (ExpandableListView) rootView.findViewById(R.id.expandableListView1);
+		exList = (ExpandableListView) rootView.findViewById(R.id.expandableListView1); //CONTEUDO DA LISTA
 		
-		List<GroupEntity> listgrupo = new ArrayList<GroupEntity>();
+		List<GroupEntity> listgrupo = new ArrayList<GroupEntity>(); //CONTEM OS TOPICOS
 		
+		/**** DEFINE LISTA DE MEDICAMENTOS ****/
 		GroupEntity grupo = new GroupEntity(1,"Medicamentos","D");
-		
 		DbHelperMedicamento db = new DbHelperMedicamento(getActivity().getApplicationContext());
 		ArrayList<Medicamento> estaTomando = db.listaMedicamentos(p);
-		List<Medicamento> child = db.listaMedicamentos();  // provavelmente aqui vem a consulta no banco
 		
-		if(child== null){
-			child = new ArrayList<Medicamento>(); //Se nao tem nenhum, cria lista vazia
+		//favoritos primeiro
+		List<Medicamento> childrenMedicamentos = db.listaMedicamentos(true);  // provavelmente aqui vem a consulta no banco
+		
+		if(childrenMedicamentos== null){
+			childrenMedicamentos = new ArrayList<Medicamento>(); //Se nao tem nenhum, cria lista vazia
 		}
 		
+		//depois nao favoritos
+		List<Medicamento> childrenMedicamentosNaoFavoritos = db.listaMedicamentos(false);  // provavelmente aqui vem a consulta no banco
+		
+		if(childrenMedicamentosNaoFavoritos != null){
+			childrenMedicamentos.addAll(childrenMedicamentosNaoFavoritos); //Adiciona a lista se houver 
+		}
+		
+		
+		/**** DEFINE LISTA DE DIAGNOSTICOS ****/
 		GroupEntity grupo2 = new GroupEntity(1,"Diagnosticos","A");
 		
 		DbHelperDiagnostico dbd = new DbHelperDiagnostico(getActivity().getApplicationContext());
@@ -75,19 +92,51 @@ public class ListaMedicamentosPorPaciente extends Fragment {
 		if(diagnosticos == null){
 			diagnosticos = new ArrayList<Diagnostico>();
 		}
-		ArrayList<Medicamento> md =new ArrayList<Medicamento>(diagnosticos.size());
+		ArrayList<Medicamento> childrenDiagnosticos =new ArrayList<Medicamento>(diagnosticos.size()); //SAO NA VERDADE DIAGNOSTICOS
 		for (Diagnostico d: diagnosticos){
-			md.add(new Medicamento(d.getId(),d.getNome()));
+			childrenDiagnosticos.add(new Medicamento(d.getId(),d.getNome()));		//SAO NA VERDADE DIAGNOSTICOS
 		}
 		
-		grupo2.setListChild(md);
-		//PARTE DE TESTE
-		//child.add(new Medicamento(1,"doril"));
-		//child.add(new Medicamento(2,"buscopan"));
 		
-		grupo.setListChild(child);
+		/**** DEFINE LISTA DE EXAMES ****/
+		GroupEntity grupo3 = new GroupEntity(1,"Exames","E");
+		
+		DbHelperExame dbdb = new DbHelperExame(getActivity().getApplicationContext());
+		LinkedList<Exame> exames = dbdb.listaExames();
+		
+		//gambiarra
+		if(exames == null){
+			exames = new LinkedList<Exame>();
+		}
+		ArrayList<Medicamento> childrenExames = new ArrayList<Medicamento>(exames.size()); //SAO NA VERDADE EXAMES
+		for (Exame e: exames){
+			childrenExames.add(new Medicamento(e.getId(),e.getNome()));		//SAO NA VERDADE EXAMES
+		}
+		
+		/*
+		 * EditText decimal = new EditText(getActivity());
+			decimal.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+			decimal.setInputType(InputType.TYPE_CLASS_NUMBER
+					| InputType.TYPE_NUMBER_FLAG_DECIMAL);
+			decimal.setHint(nome.toLowerCase());
+			decimal.setId(i);
+			id_campos.add(decimal.getId());
+			layout.addView(decimal);
+		 */
+		
+		
+		/**** ADICIONA CONTEUDO AS LISTAS ****/
+		grupo3.setListChild(childrenDiagnosticos);
+		grupo2.setListChild(childrenDiagnosticos);
+				//PARTE DE TESTE
+				//child.add(new Medicamento(1,"doril"));
+				//child.add(new Medicamento(2,"buscopan"));
+		grupo.setListChild(childrenMedicamentos);
+		
 		listgrupo.add(grupo);
 		listgrupo.add(grupo2);
+		listgrupo.add(grupo3);
+		
 		adapter = new ExpandableAdapter(getActivity(),listgrupo,estaTomando);
  		exList.setAdapter(adapter);
  		
@@ -167,6 +216,7 @@ public class ListaMedicamentosPorPaciente extends Fragment {
 	
 	public boolean salvaDados(){
 		
+		/**** MEDICAMENTO_PACIENTE ****/
 		GroupEntity c = (GroupEntity) adapter.getGroup(0);
 		
 		long id_consulta = getActivity().getIntent().getExtras().getLong("id_consulta");
@@ -182,6 +232,7 @@ public class ListaMedicamentosPorPaciente extends Fragment {
 			}
 		}
 		
+		/**** DIAGNOSTICO_CONSULTA ****/
 		c = (GroupEntity) adapter.getGroup(1);
 		
 		DbHelperDiagnostico_Consulta dbd = new DbHelperDiagnostico_Consulta(getActivity().getApplicationContext());
